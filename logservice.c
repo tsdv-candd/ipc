1e5eb2761976ff5cc7468df0a8bbcb61961969c3
candd <candd@debian-lts.debian-lts>
2015-04-23 16:43:16 +0700

    Add log function

--- logservice.c
+++ logservice.c
@@ -4,14 +4,50 @@
 int logServiceInit()
 {
 	int id;
+    key_t key;
+    int msgflg = IPC_CREAT | 0666;
+
+    key = KEY;
+    
+    fprintf(stderr, "logServiceInit: Start(%#lx,%#o)\n", key, msgflg);
+
+    /*
+     * Get the message queue id for the
+     * key = , which was created by
+     * the server.
+     */
+    if ((id = msgget(key, msgflg )) < 0) {
+        perror("logServiceInit: msgget");
+        return (-1);
+    }
 
 	return id;
-
 }
 
-int logMessage(int serviceId,char*message)
+int logMessage(int serviceId, char *message)
 {
 	int rv;
+    size_t sbuf_len;
+    struct message sbuf;
 
+    /*
+     * Create message to send.
+     */
+     
+    strcpy(sbuf.message, message);
+    sbuf_len = strlen(sbuf.message);
+    sbuf.type = getpid();
+    
+    /*
+     * Send a message.
+     */
+    if ((rv = msgsnd(serviceId, &sbuf, sbuf_len, IPC_NOWAIT)) < 0) {
+        printf ("%d, %d, %s, %d\n", serviceId, sbuf.type, sbuf.message, sbuf_len);
+        perror("logMessage: msgsnd");
+        return (-1);
+        //exit(1);
+    } 
+    
+    printf("Message: \"%s\" Sent\n", sbuf.message);
 	return rv;
 }
